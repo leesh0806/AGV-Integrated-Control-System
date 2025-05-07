c346b2abcff492fb54994afa37a7a9b1b726eab2
김대인 <kimdaren2@gmail.com>
2025-05-07 18:16:08 +0900

    GateA GateB functions splitted
    Added function for gate B separately

--- firmware/truck/rfid_gate_servo/rfid_gate_servo.ino
+++ firmware/truck/rfid_gate_servo/rfid_gate_servo.ino
@@ -1,14 +1,51 @@
-g#include <Servo.h>
+#include <Servo.h>
 
-#define GATE_SERVO_PIN 9
+#define GATE_A 9
+#define GATE_B 8
 
-Servo gateServo;
-bool isOpen = false;
+Servo gateServo_A;
+Servo gateServo_B;
 
-void setup() {
+bool AisOpen = false;
+bool BisOpen = false;
+
+
+void openGate_A() 
+{
+  gateServo_A.write(90);  // 열림 위치
+  Serial.println("🔓 게이트A 열림");
+  AisOpen = true;
+}
+
+void closeGate_A() 
+{
+  gateServo_A.write(5);  // 닫힘 위치
+  Serial.println("🔒 게이트A 닫힘");
+  AisOpen = false;
+}
+
+void openGate_B() 
+{
+  gateServo_B.write(90);  // 열림 위치
+  Serial.println("🔓 게이트B 열림");
+  BisOpen = true;
+}
+
+void closeGate_B() 
+{
+  gateServo_B.write(5);  // 닫힘 위치
+  Serial.println("🔒 게이트B 닫힘");
+  BisOpen = false;
+}
+
+
+void setup() 
+{
   Serial.begin(9600);
-  gateServo.attach(GATE_SERVO_PIN);
-  closeGate();  // 초기 상태 닫힘
+  gateServo_A.attach(GATE_A);
+  gateServo_B.attach(GATE_B);
+  closeGate_A ();  // 초기 상태 닫힘
+  closeGate_B ();  // 초기 상태 닫힘
   Serial.println("🚦 Gate Controller Ready (Test Mode)");
 }
 
@@ -18,32 +55,20 @@ void loop() {
     cmd.trim();
 
     if (cmd == "GATE_A_OPEN") {
-      openGate();
+      openGate_A();
       Serial.println("GATE_A_OPENED");
     } else if (cmd == "GATE_A_CLOSE") {
-      closeGate();
+      closeGate_A();
       Serial.println("GATE_A_CLOSED");
     } else if (cmd == "GATE_B_OPEN") {
-      openGate();
+      openGate_B();
       Serial.println("GATE_B_OPENED");
     } else if (cmd == "GATE_B_CLOSE") {
-      closeGate();
+      closeGate_B();
       Serial.println("GATE_B_CLOSED");
     } else {
       Serial.print("❓ Unknown Command: ");
       Serial.println(cmd);
     }
   }
-}
-
-void openGate() {
-  gateServo.write(90);  // 열림 위치
-  Serial.println("🔓 게이트 열림");
-  isOpen = true;
-}
-
-void closeGate() {
-  gateServo.write(0);  // 닫힘 위치
-  Serial.println("🔒 게이트 닫힘");
-  isOpen = false;
 }
